ef3ada5b9d3c60d276c06b0e4936d3037b5186da
Florent Delbey <florent.delbey@gmail.com>
2025-05-21 20:12:17 +0200

    Boss

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,172 +1,193 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     [Header("References")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource audioSource;
 
-    [SerializeField]
-    private NavMeshAgent agent;
+    [Header("Stats")]
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float chaseSpeed;
+    [SerializeField] private float detectionRadius;
+    [SerializeField] private float attackRadius;
+    [SerializeField] private float attackDelay;
+    [SerializeField] private float damageDealt;
+    [SerializeField] private float rotationSpeed;
 
-    [SerializeField]
-    private Animator animator;
+    [Header("Wandering parameters")]
+    [SerializeField] private float wanderingWaitTimeMin;
+    [SerializeField] private float wanderingWaitTimeMax;
+    [SerializeField] private float wanderingDistanceMin;
+    [SerializeField] private float wanderingDistanceMax;
 
-    [SerializeField]
-    private AudioSource audioSource;
+    [Header("Boss Throne Settings")]
+    [SerializeField] private bool isBoss = false;
+    [SerializeField] private GameObject throne;
 
     private Transform player;
     private PlayerStats playerStats;
-
-    [Header("Stats")]
-
-    [SerializeField]
-    private float maxHealth;
-
-    [SerializeField]
     private float currentHealth;
-
-    [SerializeField]
-    private float walkSpeed;
-
-    [SerializeField]
-    private float chaseSpeed;
-
-    [SerializeField]
-    private float detectionRadius;
-
-    [SerializeField]
-    private float attackRadius;
-
-    [SerializeField]
-    private float attackDelay;
-
-    [SerializeField]
-    private float damageDealt;
-
-    [SerializeField]
-    private float rotationSpeed;
-
-    [Header("Wandering parameters")]
-
-    [SerializeField]
-    private float wanderingWaitTimeMin;
-
-    [SerializeField]
-    private float wanderingWaitTimeMax;
-
-    [SerializeField]
-    private float wanderingDistanceMin;
-
-    [SerializeField]
-    private float wanderingDistanceMax;
-
     private bool hasDestination;
     private bool isAttacking;
     private bool isDead;
+    private bool isSeated = true;
+    private bool initialDetectionDone = false;
 
     private void Awake()
     {
-        Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-
-        player = playerTransform;
-        playerStats = playerTransform.GetComponent<PlayerStats>();
-
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerStats = player.GetComponent<PlayerStats>();
         currentHealth = maxHealth;
+
+        if (isBoss)
+        {
+            agent.enabled = false;
+            animator.Play("Seated_Idle");
+        }
     }
 
-    void Update()
+    private void Update()
     {
-        if(Vector3.Distance(player.position, transform.position) < detectionRadius && !playerStats.isDead)
+        if (isDead) return;
+
+        bool playerInRange = Vector3.Distance(player.position, transform.position) < detectionRadius && !playerStats.isDead;
+
+        if (isBoss && isSeated)
         {
-            agent.speed = chaseSpeed;
-
-            Quaternion rot = Quaternion.LookRotation(player.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotationSpeed * Time.deltaTime);
-
-            if(!isAttacking)
+            if (playerInRange && !initialDetectionDone)
             {
-                if (Vector3.Distance(player.position, transform.position) < attackRadius)
-                {
-                    StartCoroutine(AttackPlayer());
-                }
-                else
-                {
-                    agent.SetDestination(player.position);
-                }
+                StandUpFromThrone();
+                initialDetectionDone = true;
             }
-            
+            return;
+        }
+
+        if (playerInRange)
+        {
+            HandlePlayerDetection();
         }
         else
         {
-            agent.speed = walkSpeed;
-
-            if(agent.remainingDistance < 0.75f && !hasDestination)
-            {
-                StartCoroutine(GetNewDestination());
-            }
+            HandleWandering();
         }
 
         animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 
+    private void HandlePlayerDetection()
+    {
+        agent.speed = chaseSpeed;
+        RotateTowardsPlayer();
+
+        if (!isAttacking)
+        {
+            if (Vector3.Distance(player.position, transform.position) < attackRadius)
+            {
+                StartCoroutine(AttackPlayer());
+            }
+            else
+            {
+                agent.SetDestination(player.position);
+            }
+        }
+    }
+
+    private void HandleWandering()
+    {
+        agent.speed = walkSpeed;
+
+        if (agent.remainingDistance < 0.75f && !hasDestination)
+        {
+            StartCoroutine(GetNewDestination());
+        }
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotationSpeed * Time.deltaTime);
+    }
+
     public void TakeDammage(float damages)
     {
-        if (isDead)
-        {
-            return;
-        }
+        if (isDead) return;
 
         currentHealth -= damages;
 
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
         {
-            isDead = true;
-            animator.SetTrigger("Die");
-            agent.enabled = false;
-            enabled = false;
-        } else
+            Die();
+        }
+        else
         {
             animator.SetTrigger("GetHit");
         }
     }
 
-    IEnumerator GetNewDestination()
+    private void Die()
+    {
+        isDead = true;
+        animator.SetTrigger("Die");
+        agent.enabled = false;
+        enabled = false;
+    }
+
+    private IEnumerator GetNewDestination()
     {
         hasDestination = true;
         yield return new WaitForSeconds(Random.Range(wanderingWaitTimeMin, wanderingWaitTimeMax));
 
         Vector3 nextDestination = transform.position;
-        nextDestination += Random.Range(wanderingDistanceMin, wanderingDistanceMax) * new Vector3(Random.Range(-1f, 1), 0f, Random.Range(-1f, 1f)).normalized;
+        nextDestination += Random.Range(wanderingDistanceMin, wanderingDistanceMax) * 
+                          new Vector3(Random.Range(-1f, 1), 0f, Random.Range(-1f, 1f)).normalized;
 
-        NavMeshHit hit;
-        if(NavMesh.SamplePosition(nextDestination, out hit, wanderingDistanceMax, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(nextDestination, out NavMeshHit hit, wanderingDistanceMax, NavMesh.AllAreas))
         {
             agent.SetDestination(hit.position);
         }
         hasDestination = false;
     }
 
-    IEnumerator AttackPlayer()
+    private IEnumerator AttackPlayer()
     {
         isAttacking = true;
         agent.isStopped = true;
-
         audioSource.Play();
-
         animator.SetTrigger("Attack");
 
         playerStats.TakeDamage(damageDealt);
 
         yield return new WaitForSeconds(attackDelay);
 
-        if(agent.enabled)
+        if (agent.enabled)
         {
             agent.isStopped = false;
         }
-        
         isAttacking = false;
+    }
+
+    private void StandUpFromThrone()
+    {
+        isSeated = false;
+        animator.SetTrigger("StandUp");
+        StartCoroutine(EnableNavMeshAfterAnimation());
+
+        if (throne != null)
+        {
+            throne.GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    private IEnumerator EnableNavMeshAfterAnimation()
+    {
+        yield return new WaitForSeconds(2f);
+        agent.enabled = true;
+        animator.SetBool("IsStanding", true);
     }
 
     private void OnDrawGizmos()
